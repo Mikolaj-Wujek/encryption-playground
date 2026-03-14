8002cf25615b4922167fd1af5071d107c6ec8c17
Mikolaj-Wujek <mikolajwork1@gmail.com>
2026-03-14 18:32:59 +0000

    added Viginere

--- ciphers.py
+++ ciphers.py
@@ -18,4 +18,39 @@ def caesar_cipher(plaintext, key):
             result += char
     return result
 
-print(caesar_cipher("hello I am under the WATER HELLO", 7))
+def vigenere_cipher(plaintext, key):
+    shiftKeys = []
+    for char in key:
+        if char in lower:
+            cipherIndex = lower.index(char)
+            shiftKeys.append(cipherIndex)
+        elif char in upper:
+            cipherIndex = upper.index(char)
+            shiftKeys.append(cipherIndex)
+    completionCounter = 0
+    shiftCycleCounter = 0
+    keyLength = len(shiftKeys)
+    result = ""
+
+    while completionCounter != len(plaintext):
+        if plaintext[completionCounter] in lower:
+            cipherIndex = (lower.index(plaintext[completionCounter]) + shiftKeys[shiftCycleCounter]) % 26
+            char = lower[cipherIndex]
+            result += char
+            completionCounter += 1
+            shiftCycleCounter = (shiftCycleCounter + 1) % keyLength
+
+        elif plaintext[completionCounter] in upper:
+            cipherIndex = (upper.index(plaintext[completionCounter]) + shiftKeys[shiftCycleCounter]) % 26
+            char = upper[cipherIndex]
+            result += char
+            completionCounter += 1
+            shiftCycleCounter = (shiftCycleCounter + 1) % keyLength
+        
+        else:
+            result += plaintext[completionCounter]
+            completionCounter += 1
+    return result
+
+# print(caesar_cipher("secret Message from Above! Stay safe everyONE.", 7))
+print(vigenere_cipher("secret Message from Above! Stay safe everyONE.", "key"))
