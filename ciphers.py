09fb1b12584f1dcc668e52f14ee27d96dfb57385
Mikolaj-Wujek <mikolajwork1@gmail.com>
2026-03-14 19:13:32 +0000

    added XOR cipher

--- ciphers.py
+++ ciphers.py
@@ -52,5 +52,27 @@ def vigenere_cipher(plaintext, key):
             completionCounter += 1
     return result
 
+def XOR_cipher(plaintext, key):
+    shiftKeys = []
+    for char in key:
+        shiftKeys.append(ord(char))
+    
+    completionCounter = 0
+    shiftCycleCounter = 0
+    result = ""
+
+    while completionCounter != len(plaintext):
+        aVal = ord(plaintext[completionCounter])
+        char = chr(aVal ^ shiftKeys[shiftCycleCounter])
+        result += char
+        shiftCycleCounter = (shiftCycleCounter + 1) % len(shiftKeys)
+        completionCounter += 1
+    return result
+
 # print(caesar_cipher("secret Message from Above! Stay safe everyONE.", 7))
-print(vigenere_cipher("secret Message from Above! Stay safe everyONE.", "key"))
+# print(vigenere_cipher("secret Message from Above! Stay safe everyONE.", "key"))
+# print(XOR_cipher("Fiber Optic cable! lot of mOney in this stuff.", "ab"))
+
+encrypted = XOR_cipher("hello world", "ab")
+decrypted = XOR_cipher(encrypted, "ab")
+print(decrypted)
